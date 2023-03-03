f1ae0098f39bbe5cec987d63f7219b204a5545b5
Muslimin Ontong <moshOntong@gmail.com>
2023-03-03 21:56:57 +0800

    Update main.dart
    hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ref.watch(themeModeProvider),
       theme: FlexThemeData.light(
         scheme: FlexScheme.redWine,
